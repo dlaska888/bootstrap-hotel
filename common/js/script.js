9e9bb903c3e3d96e1285d36be53d9e7a77de36c0
dlaska <dawidlaska888@gmail.com>
2023-05-08 00:39:30 +0200

    added on scroll hiding bar and proper photo resize

--- common/js/script.js
+++ common/js/script.js
@@ -2,14 +2,34 @@
 var navbarToggleBtn = document.getElementById("navbar-toggle-btn");
 var navItems = document.getElementsByClassName("nav-item");
 
-var closeMenu = function() {
-    if (document.getElementById("navbarSupportedContent").classList.contains("show")) {
+var closeMenu = function () {
+    if (
+        document
+            .getElementById("navbarSupportedContent")
+            .classList.contains("show")
+    ) {
         navbarToggleBtn.click();
     }
 };
 
+// close on click outside
+navbarToggleBtn.addEventListener("focusout", () => closeMenu());
+
+// close on click navbar item
 for (var i = 0; i < navItems.length; i++) {
-    navItems[i].addEventListener('click', closeMenu, false);
+    navItems[i].addEventListener("click", closeMenu, false);
 }
 
-navbarToggleBtn.addEventListener('focusout', () => closeMenu());
+// close on scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("top-navbar").style.top = "0px";
+  } else {
+    document.getElementById("top-navbar").style.top = "-10rem";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// end navbar closing logic
